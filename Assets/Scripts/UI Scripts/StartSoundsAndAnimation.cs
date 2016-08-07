71c049225324b6309067bf2bbef42f0e24296f9d
Gwen <techforthat@gmail.com>
2016-08-07 04:54:52 -0700

    Revert "Revert "Buncha stuff, start screen nav mesh""
    This reverts commit a766c9e97b0adc7ceef15e63d5a2c48802160ffa.

---
+++ Assets/Scripts/UI Scripts/StartSoundsAndAnimation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartSoundsAndAnimation : MonoBehaviour {
+    public bool triggerFall;
+    public bool triggercrash;
+
+    public Transform bullshitThing;
+
+    public AudioClip whistle;
+    public AudioClip crash;
+
+    bool called;
+    bool secondcalled;
+    void Update()
+    {
+        if (triggerFall)
+        {
+            if (!secondcalled)
+            {
+                secondcalled = true;
+                transform.GetComponent<AudioSource>().PlayOneShot(whistle);
+            }
+            //
+            triggerFall = false;
+        }
+        if (triggercrash)
+        {
+            if (!called)
+            {
+                called = true;
+                transform.GetComponent<AudioSource>().PlayOneShot(crash);
+            }
+            bullshitThing.GetComponent<Animation>().CrossFade("Dead", 0.01f);
+            
+            triggercrash = false;
+
+        }
+
+    }
+	
+
+}
