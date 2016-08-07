026adec45e2679973945de95328ccabd875840ee
BlueRider0 <adanielson@gmail.com>
2016-08-07 04:53:52 -0700

    Buncha stuff, start screen nav mesh

---
+++ Assets/Scripts/UI Scripts/UIWait.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIWait : MonoBehaviour {
+    public float Wait;
+
+    void Start()
+    {
+        StartCoroutine(mainth());
+    }
+
+    IEnumerator mainth()
+    {
+
+        yield return new WaitForSeconds(Wait);
+        transform.GetComponent<Canvas>().enabled = true;
+
+    }
+
+    public void LoadThing(string k)
+    {
+        if ((k != "") && (k.ToLower() != "quit"))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+}
